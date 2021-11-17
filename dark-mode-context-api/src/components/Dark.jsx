7c7b110fd196c1f783a843ae300651b0d60ae194
Mansur Dewan <mansurdewan26@gmail.com>
2021-11-17 22:09:17 +0530

    dark mdoe

---
+++ dark-mode-context-api/src/components/Dark.jsx
@@ -0,0 +1,72 @@
+
+import styled from "styled-components";
+import {DarkUser} from "./DarkUser";
+
+const Dark = () => {
+
+
+
+    const Wrapper = styled.div`
+        position: absolute;
+        left: 8.47%;
+        right: 70.14%;
+        top: 11.04%;
+        bottom: 38.77%;
+        width: 500px;
+        padding: 30px;
+        
+        // background: black;
+        box-shadow: 0px 1px 4px #E5E9F2;
+        border-radius: 5px;
+    `
+    let obj = {
+        name : "Nrupul Dev",
+        add : "Banglore, India",
+        level : "Professional Level 15",
+        points : "4773",
+        color: "#29CB97",
+        width: "70%", 
+        image : "https://th.bing.com/th/id/OIP.HEto-JEMp9Amgfx4r1zTdwHaKi?pid=ImgDet&w=149&h=211&c=7&dpr=1.75"
+    }
+    let obj2 = {
+        name : "Prateek Dev",
+        add : "Banglore, India",
+        level : "Professional Level 11",
+        points : "3223",
+        color : "#4072EE",
+        width: "50%", 
+        image : "https://image.cnbcfm.com/api/v1/image/106216476-1572545404537gettyimages-1151703682.jpeg?v=1572545446"
+    }
+    let obj3 = {
+        name : "Mark Dev",
+        add : "Banglore, India",
+        level : "Professional Level 11",
+        points : "233",
+        width: "30%", 
+        color: "#B558F6",
+        image : "https://image.cnbcfm.com/api/v1/image/106216476-1572545404537gettyimages-1151703682.jpeg?v=1572545446"
+    }
+    const Active = styled.div`
+        width: 88%;
+        height: 100px;
+        padding: 30px;
+        color: white;
+        background-color: black;
+    `
+
+    return (
+        <Wrapper>
+            <Active>
+                <div style = {{float: "left"}}>Active Users</div>
+                <div style = {{float : "right"}}>for August 2020</div>
+            </Active>
+            <div style = {{marginTop: "-100px"}}>
+                <DarkUser data = {obj} />
+                <DarkUser data = {obj2}/>
+                <DarkUser data = {obj3} />
+            </div>
+
+        </Wrapper>
+    )
+}
+export {Dark};
---
+++ dark-mode-context-api/src/components/Left.jsx
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+
+const Left = () => {
+
+    const Wrapper = styled.div`
+    position: absolute;
+    left: 0%;
+    right: 94.38%;
+    top: 0%;
+    bottom: 0%;
+    width: 81px;
+    height: 1024px;
+    
+    background: #FFFFFF;
+    box-shadow: 0px 0px 5px #E5E9F2;
+    `
+    const Image = styled.div`
+    width: 40px;
+    height: 40px;
+
+     background : url("https://3.bp.blogspot.com/-ZDmUhMfvLCI/UeVxlchEg8I/AAAAAAAAnfY/t1FEyxjOIzw/s1600/Shahrukh+Khan+Latest+HD+Wallpapers+1.jpg");
+    border-radius: 50%;
+    background-repead : no-repeat;
+    background-size : 40px 40px;
+    margin-left: 20px;
+    margin-top: 20px;
+    float: left;
+    `
+    const Top = styled.div`
+    
+    `
+    const Middle = styled.div`
+        // border: 1px solid black;
+        margin-top: 200px;
+        
+    `
+
+
+
+
+
+    const Bottom = styled.div`
+        margin-top: 200px;
+    `
+    return (
+        <Wrapper>
+            <Top>
+                <Image />
+
+            </Top>
+            <Middle>
+                <div style = {{textAlign: 'center'}}><img style = {{backgroundColor: "#679CF6", width: "81px", height: "70px", borderRadius: "5px"}} src = "https://cdn-icons.flaticon.com/png/128/2652/premium/2652712.png?token=exp=1637164225~hmac=f41a7aef3f04039befb6065b91f55505" alt = "network" /></div>
+                <div style = {{textAlign: 'center'}}><img style = {{ width: "31px", height: "40px", borderRadius: "5px"}} src = "https://img.icons8.com/external-kmg-design-flat-kmg-design/72/external-sign-post-maps-navigation-kmg-design-flat-kmg-design.png" alt = "network" /></div>
+                <div style = {{textAlign: 'center'}}><img style = {{ width: "31px", height: "40px", borderRadius: "5px"}} src = "https://cdn-icons.flaticon.com/png/128/2022/premium/2022197.png?token=exp=1637164905~hmac=4e26810d78e6617dd2383a81a97da1e4" alt = "network" /></div>
+                <div style = {{textAlign: 'center'}}><img style = {{ width: "31px", height: "40px", borderRadius: "5px"}} src = "https://cdn-icons.flaticon.com/png/128/2594/premium/2594058.png?token=exp=1637164995~hmac=61a06da64caa855f7e21ad4e43223483" alt = "network" /></div>
+                <div style = {{textAlign: 'center'}}><img style = {{ width: "51px", height: "40px", borderRadius: "5px"}} src = "https://t4.ftcdn.net/jpg/01/26/09/55/240_F_126095599_eFIP8OFYe2lZXCeaAqKsDFBWIoUqqqxL.jpg" alt = "network" /></div>
+                <div style = {{textAlign: 'center'}}><img style = {{ width: "51px", height: "40px", borderRadius: "5px"}} src = "https://t4.ftcdn.net/jpg/01/13/94/83/240_F_113948390_gRY4UwSTxm2bNX8jD2oIjpEuwJPELTTr.jpg" alt = "network" /></div>
+            </Middle>
+            <Bottom>
+            <div style = {{textAlign: 'center'}}><img style = {{ width: "51px", height: "40px", borderRadius: "5px"}} src = "https://cdn-icons-png.flaticon.com/128/190/190250.png" alt = "network" /></div>
+
+            </Bottom>
+        
+        </Wrapper>
+    )
+}
+
+export {Left};
